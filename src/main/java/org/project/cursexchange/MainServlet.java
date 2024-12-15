2d65b1907219e99395a5ec10da6e5f49a94ca93c
Misha Kasyanov <103356933+fernerman@users.noreply.github.com>
2024-12-16 00:03:15 +0500

    create adding a new currency and getting one by code

--- src/main/java/org/project/cursexchange/MainServlet.java
+++ src/main/java/org/project/cursexchange/MainServlet.java
@@ -2,47 +2,80 @@ package org.project.cursexchange;
 
 import com.google.gson.Gson;
 import org.project.cursexchange.dao.CurrencyDao;
+import org.project.cursexchange.dto.CurrencyDto;
 import org.project.cursexchange.models.Currency;
-import org.project.cursexchange.models.ExchangeCurrency;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.SQLException;
 import java.util.List;
+import java.util.Optional;
 
-@WebServlet("/currencies")
+@WebServlet("/currencies/*")
 public class MainServlet extends HttpServlet {
 
-    private void proccessResponse(HttpServletResponse response, String payload) throws ServletException, IOException {
+    private void proccessResponse(HttpServletResponse response, Object objectToJson) throws IOException {
         response.setHeader("Content-Type", "application/json");
         response.setCharacterEncoding("UTF-8");
-        try{
-           response.setStatus(HttpServletResponse.SC_OK);
-            if(payload != null){
-                PrintWriter out = response.getWriter();
-                out.print(payload);
-                out.flush();
+        String payload = new Gson().toJson(objectToJson);
+        PrintWriter out = response.getWriter();
+        out.print(payload);
+        out.flush();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        try {
+            String path = request.getPathInfo();
+            if(path==null) {
+                List<Currency> allCurrency=new CurrencyDao().findAll();
+                proccessResponse(response, allCurrency);
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            else if(path.trim().equals("/")) {
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            }
+            else{
+                String code=path.split("/")[1];
+                Optional<Currency> d=new CurrencyDao().findByCode(code);
+                if(d.isPresent()) {
+                    proccessResponse(response, d.get());
+                    response.setStatus(HttpServletResponse.SC_OK);
+                }
+                else {
+                    response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                }
+            }
+        }
+        catch (Exception e) {
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            proccessResponse(response, e.getMessage());
         }
     }
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        StringBuilder stringBuilder=new StringBuilder();
+    protected void doPost(HttpServletRequest request,HttpServletResponse response) throws IOException {
         try {
-            List<Currency> allCurrency=new CurrencyDao().findAll();
-            stringBuilder.append(new Gson().toJson(allCurrency));
-            proccessResponse(response, stringBuilder.toString());
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+            String code = request.getParameter("code");
+            String name = request.getParameter("name");
+            String sign = request.getParameter("sign");
+
+            CurrencyDto currencyDto = new CurrencyDto(code, name, sign);
+            Optional<Currency> currencySaved =new CurrencyDao().save(currencyDto.toEntity());
+            if(currencySaved.isEmpty()){
+                response.setStatus(HttpServletResponse.SC_CONFLICT);
+
+            }
+            else {
+                response.setStatus(HttpServletResponse.SC_CREATED);
+                proccessResponse(response, currencySaved.get());
+            }
         }
+        catch (Exception e) {
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            proccessResponse(response, e.getMessage());
+        }
+
     }
-
-
 }
