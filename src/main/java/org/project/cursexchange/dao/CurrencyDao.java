a22601b1cc2f050fc4668683fb7cb0c5fe87b72e
Misha Kasyanov <103356933+fernerman@users.noreply.github.com>
2024-12-16 00:08:00 +0500

    write sql requests

--- src/main/java/org/project/cursexchange/dao/CurrencyDao.java
+++ src/main/java/org/project/cursexchange/dao/CurrencyDao.java
@@ -14,11 +14,36 @@ import java.util.Optional;
 public class CurrencyDao implements Dao<Currency> {
     private final String nameTable = "Currencies";
     @Override
-    public Optional<Currency> findById(int id) {
-
+    public Optional<Currency> findById(int id) throws SQLException {
         return Optional.empty();
     }
 
+    public List<Currency> getCurrenciesFromSet(ResultSet resultSet) throws SQLException {
+        List<Currency> currencies = new ArrayList<>();
+        while (resultSet.next()) {
+            int id=resultSet.getInt("id");
+            String code=resultSet.getString("Code");
+            String name=resultSet.getString("FullName");
+            String sign=resultSet.getString("Sign");
+            Currency currency=new Currency((long) id,code,name,sign);
+            currencies.add(currency);
+        }
+        return  currencies;
+    }
+
+    @Override
+    public Optional<Currency> findByCode(String code) throws SQLException {
+        String sql = "SELECT * FROM "+ nameTable+ " WHERE code = ?";
+        Connection connection=Database.getConnection();
+        PreparedStatement stmt=connection.prepareStatement(sql);
+        stmt.setString(1, code);
+        ResultSet resultSet=stmt.executeQuery();
+        List<Currency> listOfCurrencies =getCurrenciesFromSet(resultSet);
+        if(!listOfCurrencies.isEmpty()){
+            return  Optional.ofNullable(listOfCurrencies.get(listOfCurrencies.size()-1));
+        }
+        return Optional.empty();
+    }
 
     @Override
     public List<Currency> findAll() throws SQLException {
@@ -28,20 +53,26 @@ public class CurrencyDao implements Dao<Currency> {
         connection.setAutoCommit(false);
         PreparedStatement preparedStatement = connection.prepareStatement(sql);
         ResultSet resultSet = preparedStatement.executeQuery();
-        while (resultSet.next()) {
-                int id=resultSet.getInt("id");
-                String code=resultSet.getString("Code");
-                String name=resultSet.getString("FullName");
-                String sign=resultSet.getString("Sign");
-                Currency currency=new Currency(id,code,name,sign);
-                currencies.add(currency);
-        }
-        return currencies;
+        return getCurrenciesFromSet(resultSet);
     }
 
     @Override
-    public void save(Currency currency) {
-
+    public Optional<Currency> save(Currency currency) throws SQLException {
+        Connection connection= Database.getConnection();
+        String sqlRequest = "INSERT INTO " + nameTable + " (Code, FullName, Sign) VALUES (?, ?, ?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(sqlRequest);
+        preparedStatement.setString(1, currency.getCode());
+        preparedStatement.setString(2, currency.getName());
+        preparedStatement.setString(3, currency.getSign());
+        Optional<Currency> currencyByCodeExisting=findByCode(currency.getCode());
+        if(currencyByCodeExisting.isEmpty()){
+            preparedStatement.executeUpdate();
+            currencyByCodeExisting=findByCode(currency.getCode());
+            return currencyByCodeExisting;
+        }
+        else {
+            return Optional.empty();
+        }
     }
 
     @Override
