6d7d3b30cc8f18c4dea845df9e26c78f739efd27
Misha Kasyanov <103356933+fernerman@users.noreply.github.com>
2025-01-16 22:43:44 +0500

    refactor:add lombock

--- src/main/java/org/project/cursexchange/model/Currency.java
+++ src/main/java/org/project/cursexchange/model/Currency.java
@@ -1,47 +1,15 @@
 package org.project.cursexchange.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Currency {
     private int id;
     private String code;
     private String name;
     private String sign;
-
-    public Currency(int id, String code, String name, String sign) {
-        this.id = id;
-        this.code = code;
-        this.name = name;
-        this.sign = sign;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSign() {
-        return sign;
-    }
-
-    public void setSign(String sign) {
-        this.sign = sign;
-    }
 }
--- src/main/java/org/project/cursexchange/model/ExchangeRate.java
+++ src/main/java/org/project/cursexchange/model/ExchangeRate.java
@@ -1,49 +1,18 @@
 package org.project.cursexchange.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
 
 public class ExchangeRate {
     private int id;
     private Currency baseCurrency;
     private Currency targetCurrency;
     private BigDecimal rate;
-
-    public ExchangeRate(int id, Currency baseCurrency, Currency targetCurrency, BigDecimal rate) {
-        this.id = id;
-        this.baseCurrency = baseCurrency;
-        this.targetCurrency = targetCurrency;
-        this.rate = rate;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public Currency getBaseCurrency() {
-        return baseCurrency;
-    }
-
-    public void setBaseCurrency(Currency baseCurrency) {
-        this.baseCurrency = baseCurrency;
-    }
-
-    public Currency getTargetCurrency() {
-        return targetCurrency;
-    }
-
-    public void setTargetCurrency(Currency targetCurrency) {
-        this.targetCurrency = targetCurrency;
-    }
-
-    public BigDecimal getRate() {
-        return rate;
-    }
-
-    public void setRate(BigDecimal rate) {
-        this.rate = rate;
-    }
 }
